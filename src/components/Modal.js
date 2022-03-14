fcc6a71218f97c9cdfa9c63bce4a00af96e6627d
Sara Romão <sara-romao@live.com>
2022-03-14 11:37:00 -0300

    inicio modal

---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Modal.module.css'
+
+export default function Modal() {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}>
+
+      </div>
+    </div>
+  )
+}
