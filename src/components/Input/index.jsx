722aa49de09ceb0023e54a5901dc568a4dd0f21e
Sara Romão <sara-romao@live.com>
2022-06-11 18:31:27 -0300

    animação

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -7,7 +7,7 @@ export function Input({title, id, value, setValue, mask}) {
 
  
   return (
-    <div>
+    <div className='input'>
     <label htmlFor={id}>{title}</label><br/>
     <InputMask mask={mask} type='text' id={id} value={value} onChange={({target}) => setValue(target.value)} required />
    </div>
