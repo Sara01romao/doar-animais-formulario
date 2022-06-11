45e27bc06c57bb63d005576e8b3ed2ff2f06d336
Sara Romão <sara-romao@live.com>
2022-06-11 18:44:04 -0300

    identação

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,15 +1,19 @@
-
-import React from 'react';
+import React from "react";
 import InputMask from "react-input-mask";
 
-
-export function Input({title, id, value, setValue, mask}) {
-
- 
+export function Input({ title, id, value, setValue, mask }) {
   return (
-    <div className='input'>
-    <label htmlFor={id}>{title}</label><br/>
-    <InputMask mask={mask} type='text' id={id} value={value} onChange={({target}) => setValue(target.value)} required />
-   </div>
-  )
+    <div className="input">
+      <label htmlFor={id}>{title}</label>
+      <br />
+      <InputMask
+        mask={mask}
+        type="text"
+        id={id}
+        value={value}
+        onChange={({ target }) => setValue(target.value)}
+        required
+      />
+    </div>
+  );
 }
